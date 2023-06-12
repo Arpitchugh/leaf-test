68064f6c3f094a6c94b4c46c147bf5a87dcaae6a
Arpit Chugh <arpitchugh.jbd.ac@gmail.com>
2023-06-12 20:49:39 +0530

    some implementation still pending

--- src/components/player-info/index.js
+++ src/components/player-info/index.js
@@ -1,44 +1,79 @@
-import React from "react";
-import players1 from "../../players.json";
+import React, { useState } from 'react';
+import players1 from '../../players.json';
+
 export default function PlayerDetail({ close, i, addPlayer, selectedPlayers }) {
-	const [players] = React.useState([...players1]);
+	const [players] = useState([...players1]);
+	const [player, setPlayer] = useState(players[i]);
+
+	// Update the player details when the index changes
+	React.useEffect(() => {
+		setPlayer(players[i]);
+	}, [i, players]);
+
 	return (
-		//Style fixed to center
 		<div
-			className="card outlined mt-0"
+			className='card outlined mt-0'
 			style={{
-				position: "fixed",
-				left: "50%",
-				transform: "translateX(-50%)",
-				padding: "20px",
-				width: "500px",
-				top: "30%",
+				position: 'fixed',
+				left: '50%',
+				transform: 'translateX(-50%)',
+				padding: '20px',
+				width: '500px',
+				top: '30%',
 			}}
-			data-testid={`player-Rohit-Sharma-details`}
+			data-testid={`player-${player.name.split(' ').join('-')}-details`}
 		>
-			<h1 className="card-title" style={{ textAlign: "center" }}>
+			<h1 className='card-title' style={{ textAlign: 'center' }}>
 				Player Detail
 			</h1>
 			<p>
-				<strong>Name:</strong> <span data-testid="player-detail-Rohit-Sharma-name">Rohit Sharma</span>
+				<strong>Name:</strong>{' '}
+				<span
+					data-testid={`player-detail-${player.name.split(' ').join('-')}-name`}
+				>
+					{player.name}
+				</span>
 			</p>
 			<p>
-				<strong>Type:</strong> <span data-testid="player-detail-Rohit-Sharma-type">Batsman</span>
+				<strong>Type:</strong>{' '}
+				<span
+					data-testid={`player-detail-${player.name.split(' ').join('-')}-type`}
+				>
+					{player.type}
+				</span>
 			</p>
 			<p>
-				<strong>Batting:</strong> <span data-testid="player-detail-Rohit-Sharma-batting">90</span>
+				<strong>Batting:</strong>{' '}
+				<span
+					data-testid={`player-detail-${player.name
+						.split(' ')
+						.join('-')}-batting`}
+				>
+					{player.batting}
+				</span>
 			</p>
 			<p>
-				<strong>Bowling:</strong> <span data-testid="player-detail-Rohit-Sharma-bowling">90</span>
+				<strong>Bowling:</strong>{' '}
+				<span
+					data-testid={`player-detail-${player.name
+						.split(' ')
+						.join('-')}-bowling`}
+				>
+					{player.bowling}
+				</span>
 			</p>
 			<button
 				disabled={false}
-				onClick={() => addPlayer(0)}
-				data-testid="player-detail-Rohit-Sharma-add"
+				onClick={() => addPlayer(i)}
+				data-testid={`player-detail-${player.name.split(' ').join('-')}-add`}
 			>
 				Select
 			</button>
-			<button onClick={close} className="danger" data-testid="player-detail-Rohit-Sharma-close">
+			<button
+				onClick={close}
+				className='danger'
+				data-testid={`player-detail-${player.name.split(' ').join('-')}-close`}
+			>
 				Close
 			</button>
 		</div>
